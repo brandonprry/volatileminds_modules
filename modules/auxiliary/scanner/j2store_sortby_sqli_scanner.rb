0b7e4da143f0289b6c621c88dc8e84df2359e081
Brandon Perry <bperry.volatile@gmail.com>
2017-04-23 09:51:31 -0500

    rename class

--- modules/auxiliary/scanner/j2store_sortby_sqli_scanner.rb
+++ modules/auxiliary/scanner/j2store_sortby_sqli_scanner.rb
@@ -5,7 +5,7 @@
 
 require 'msf/core'
 
-class Metasploit4 < Msf::Auxiliary
+class MetasploitModule < Msf::Auxiliary
 
   # Exploit mixins should be called first
   include Msf::Exploit::Remote::HttpClient
