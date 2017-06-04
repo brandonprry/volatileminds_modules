539d9a8a5ecfed54ad75531f270d116a8350ede1
Brandon Perry <bperry.volatile@gmail.com>
2017-06-04 17:09:38 -0500

    jasper reports

---
+++ modules/auxiliary/scanner/jasperreports_scanner.rb
@@ -0,0 +1,78 @@
+##
+# This module requires Metasploit: http://metasploit.com/download
+# Current source: https://github.com/rapid7/metasploit-framework
+##
+
+require 'msf/core'
+
+class MetasploitModule < Msf::Auxiliary
+
+  # Exploit mixins should be called first
+  include Msf::Exploit::Remote::HttpClient
+  # Scanner mixin should be near last
+  include Msf::Auxiliary::Scanner
+  include Msf::Auxiliary::Report
+
+  def initialize
+    super(
+      'Name'        => 'JasperReports Scanner',
+      'Description' => %q{
+This module scans for instances of JasperReports.
+
+JasperReports is a popular enterprise solution for creating
+and managing dynamic reports from any kind of dataset. Gaining
+access to a JasperReports instance may yield significant insight
+into a company's data, and even remote access to the server via
+the operating system shell.
+
+Categories: Enterprise
+
+Price: 0
+
+Video: none
+
+OS: Multi
+
+Arch: Multi
+
+Requirements: Metasploit Framework
+      },
+      'Author'       => [],
+      'License'     => 'VolatileMinds'
+    )
+
+    register_options(
+      [
+        OptString.new('PATH', [ true,  "The test path to find JasperReports", '/jasperserver/']),
+      ], self.class)
+
+  end
+
+  def run_host(target_host)
+    res = send_request_cgi({
+      'uri' => datastore['PATH'] + '/login.html',
+    })
+
+    if res && res.body =~ /<title>TIBCO Jaspersoft: Login<\/title>/
+      str = ' - Found JasperReports'
+
+      res.body =~ /<p class="message">Product Version: <span class="emphasis">(.*?)<\/span><\/p>/
+      v = $1
+
+      res.body =~ /<p class="message">Build: <span class="emphasis">(.*?)<\/span><\/p>/
+      b = $1
+
+      if v && b
+        str << " Version #{v} Build #{b}"
+      end
+
+      print_good("#{peer}" + str)
+      report_service({
+        host: target_host,
+        port: datastore['RPORT'],
+        name: 'JasperReports',
+        info: "JasperReports #{v} #{b}"
+      })
+    end
+  end
+end
